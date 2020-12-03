35b878c5497089008b3f395768f40b3345fe8bbd
dheeraj755148 <dheerajdayakaran95@gmail.com>
2020-12-03 20:57:45 +0530

    toggle added

--- JS/dark.js
+++ JS/dark.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('theme-toggle');
     themeToggle.addEventListener('click', () => {
         if(themeStylesheet.href.includes('index')){
-            console.log('darkmode');
             themeStylesheet.href = './Style/darkIndex.css';
             themeToggle.innerText = 'Switch to light mode';
             document.getElementById('newSection').src="./Images/pira light.svg";
@@ -22,16 +21,15 @@ document.addEventListener('DOMContentLoaded', () => {
             document.getElementById('immune').src="./Images/immunelight.svg";
             document.getElementById('pirate').src="./Images/piratelight.svg"
         } else {
-            console.log('loghtmode');
 
-            themeStylesheet.href = './Style/index.css';
+            themeStylesheet.href = './Style/index.css'; 
             themeToggle.innerText = 'Switch to dark mode';
             document.getElementById('newSection').src="./Images/piranaha.svg";
             document.getElementById('newSection1').src="./Images/piranaha.svg";
             document.getElementById('newSection2').src="./Images/piranaha.svg";
             document.getElementById('anchor').src="./Images/anchor.svg";
             document.getElementById('corona').src="./Images/corona.png";
-            document.getElementById('immune').src="./Images/immune.svg";
+            document.getElementById('immune').src="./Images/immune.png";
             document.getElementById('pirate').src="./Images/pirate.png" 
         }
         localStorage.setItem('theme',themeStylesheet.href)  
